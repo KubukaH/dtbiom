357ed3d5014693ee942ff9d2f7b7a40ed469374e
KubukaH <kubukahub@gmail.com>
2022-08-03 22:13:28 +0200

    fixing buttons-margin

--- src/components/comming/index.js
+++ src/components/comming/index.js
@@ -15,7 +15,7 @@ export function CommingSoon() {
           </p>
 
           <div className="flex flex-wrap justify-center gap-4 mt-8">
-            <a className="relative inline-block text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=afg-mg1O6uY" target="__blank">
+            <a className="relative inline-flex text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=afg-mg1O6uY" target="__blank">
               <span class="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
               <span className="block inline-flex transition-transform items-center w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded-full shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring group-hover:-translate-x-1 group-hover:-translate-y-1">
                 Aluse... Mushe Mushe
@@ -24,7 +24,7 @@ export function CommingSoon() {
                 </svg>
               </span>
             </a>
-            <a className="relative inline-block text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=Ooqqq7_nmeg" target="__blank">
+            <a className="relative inline-flex text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=Ooqqq7_nmeg" target="__blank">
               <span class="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
               <span className="block inline-flex transition-transform items-center w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded-full shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring group-hover:translate-x-1 group-hover:-translate-y-1">
                 Baama aTaata
@@ -33,7 +33,7 @@ export function CommingSoon() {
                 </svg>
               </span>
             </a>
-            <a className="relative inline-block text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=eUKjnhZJFl0" target="__blank">
+            <a className="relative inline-flex text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=eUKjnhZJFl0" target="__blank">
               <span class="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
               <span className="block inline-flex transition-transform items-center w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded-full shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring group-hover:-translate-x-1 group-hover:translate-y-1">
                 Kujata jata
