c3c47c368fb0fb4d41d23ccde6f7ec2f70218d3d
KubukaH <kubukahub@gmail.com>
2022-08-03 23:02:29 +0200

    adding favicon

--- src/components/comming/index.js
+++ src/components/comming/index.js
@@ -4,7 +4,7 @@ export function CommingSoon() {
       <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
         <div className="max-w-xl mx-auto text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Under Construction.
+            Under Construction. <br />
             <strong className="font-extrabold text-red-700 sm:block">
               Back Soon.
             </strong>
@@ -16,7 +16,7 @@ export function CommingSoon() {
 
           <div className="flex flex-wrap justify-center gap-4 mt-8">
             <a className="relative inline-flex text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=afg-mg1O6uY" target="__blank">
-              <span class="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
+              <span className="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
               <span className="block inline-flex transition-transform items-center w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded-full shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring group-hover:-translate-x-1 group-hover:-translate-y-1">
                 Aluse... Mushe Mushe
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-3" viewBox="0 0 20 20" fill="currentColor">
@@ -25,7 +25,7 @@ export function CommingSoon() {
               </span>
             </a>
             <a className="relative inline-flex text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=Ooqqq7_nmeg" target="__blank">
-              <span class="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
+              <span className="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
               <span className="block inline-flex transition-transform items-center w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded-full shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring group-hover:translate-x-1 group-hover:-translate-y-1">
                 Baama aTaata
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-3" viewBox="0 0 20 20" fill="currentColor">
@@ -34,7 +34,7 @@ export function CommingSoon() {
               </span>
             </a>
             <a className="relative inline-flex text-sm font-medium text-white group focus:outline-none focus:ring" href="https://www.youtube.com/watch?v=eUKjnhZJFl0" target="__blank">
-              <span class="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
+              <span className="absolute inset-0 border rounded-full border-red-600 group-active:border-red-500"></span>
               <span className="block inline-flex transition-transform items-center w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded-full shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring group-hover:-translate-x-1 group-hover:translate-y-1">
                 Kujata jata
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-3" viewBox="0 0 20 20" fill="currentColor">
