53b3ea654c25fdfebcfffa671e4b3b5508be0df2
KubukaH <kubukahub@gmail.com>
2023-05-10 18:12:54 +0200

    fixed items

--- src/head/homeHero.js
+++ src/head/homeHero.js
@@ -6,14 +6,14 @@ export function HeadHero() {
   const img2 = new URL(headershape, import.meta.url);
 
   return (
-    <div id="home" className="relative z-10 header-hero bg-[url('../src/assets/imgs/bio-mud-bg.jpg')] bg-cover sm:bg-center md:bg-no-repeat top-0">
+    <div id="home" className="relative z-10 header-hero bg-[url('../src/assets/imgs/bio-mud-bg.jpg')] bg-cover bg-center bg-no-repeat top-0">
       <div className="px-5 mx-auto justify-center">
         <div className="row-auto">
           <div className="w-full lg:w-5/6 xl:w-2/3">
-            <div className="pt-60 pb-64 text-left">
+            <div className="pt-60 pb-64 xs:pt-24 text-left">
               <h3 className="px-5 mb-5 text-3xl font-semibold leading-tight text-white md:text-5xl">D.T. BiO Mudimba Music.</h3>
               <p className="px-5 mb-10 text-xl text-white">Musician, guitarist, music studio producer, vocals coach, sound engineer and leader of the Kaani Stars Band.</p>
-              <ul className="flex flex-wrap justify-start px-5">
+              <ul className="flex flex-wrap justify-start px-5 sm:px-2">
                 <li><a className="mx-3 main-btn gradient-btn" href="#">GET IN TOUCH</a></li>
                 <MyModal />
               </ul>
