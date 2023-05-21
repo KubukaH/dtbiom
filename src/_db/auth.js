db321a537bc39e7c697aaaa03d2a39a4d67bc7cb
KubukaH <kubukahub@gmail.com>
2023-05-21 14:08:59 +0200

    fixing the route call

--- src/_db/auth.js
+++ src/_db/auth.js
@@ -1,7 +1,7 @@
 import GoTrue from 'gotrue-js';
 
 const loc = document.location;
-const netlifyURL = `${loc.protocol}//${loc.hostname}.netlify.app/.netlify/identity`;
+const netlifyURL = `${loc.protocol}//${loc.hostname}/.netlify/identity`;
 
 const auth_strategy = new GoTrue({
   APIUrl: netlifyURL,
